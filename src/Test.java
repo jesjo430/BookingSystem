cd52e1cf51f528504ef33c50c8abe088a77856f3
jesjo430 <jesper_jonsson@hotmail.se>
2016-03-17 23:16:27 +0100

    blaj

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,6 @@ public class Test
     public static void main(String[] args) {
 	Section test = new Section(10, 20);
 
-	String text = ChairToTextConverter.convertToText(test);
-	System.out.println(text);
+	new WindowFrame(test);
     }
 }
--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -9,7 +9,7 @@ public class WindowFrame extends JFrame
     private static final int WINDOW_HEIGHT = 600;
     private static final int WINDOW_WIDTH = 900;
 
-    public WindowFrame() {
+    public WindowFrame(Section section) {
 	super("MyFrame");
 	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
